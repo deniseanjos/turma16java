935d127a059fc45d049877900ecc72b150768e4e
Denise Anjos <deniseb.anjos@outlook.com>
2021-01-27 11:34:39 -0300

    Alteração no exercicio ParImpar

--- java/ProjetoJava/src/ExerciciosSala/ParImpar.java
+++ java/ProjetoJava/src/ExerciciosSala/ParImpar.java
@@ -16,13 +16,16 @@ public class ParImpar {
 			System.out.println("Você digitou um número neutro.");
 		}
 		else if (numero % 2 > 0 && numero > 0) {
-			System.out.println("Esse número é impar.");
+			System.out.println("Esse número positivo é impar.");
 		}
 		else if (numero % 2 == 0 && numero > 0) {
-			System.out.println("Esse número é par.");
+			System.out.println("Esse número positivo é par.");
 		}
-		else if (numero < 0) {
-			System.out.println("Esse número é negativo.");
+		else if (numero % 2 < 0 && numero < 0) {
+			System.out.println("Esse número negativo é impar.");
+		}
+		else if (numero % 2 == 0 && numero < 0) {
+			System.out.println("Esse número negativo é par.");
 		}
 	}
 }
